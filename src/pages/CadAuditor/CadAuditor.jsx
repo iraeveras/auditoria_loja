6f2b8df34f63eb3ea56185d9383cdb2a835490da
Irae Veras <irae.online@gmail.com>
2024-06-24 01:24:54 -0300

    criado campos para página de cadastro de auditor

--- src/pages/CadAuditor/CadAuditor.jsx
+++ src/pages/CadAuditor/CadAuditor.jsx
@@ -1,8 +1,28 @@
+import { Link } from "react-router-dom";
+import PageHeader from "../../Components/PageHeader/PageHeader";
 import "./CadAuditor.css";
 
 function CadAuditor() {
     return (
-        <></>
+        <>
+            <PageHeader title="Gerenciamento de auditor" subtitle="Adicionar auditor" />
+            <div className="container">
+                <div className="cadastro_body">
+                    <div className="row">
+                        <div className="input_group">
+                            <label htmlFor="text_name">Nome do auditor</label>
+                            <input type="text" id="text_name" />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="btn_group">
+                            <Link href="#" className="btn btn_submit">Enviar</Link>
+                            <Link href="#" className="btn btn_cancel">Cancelar</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
