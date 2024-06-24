21b4fb55627c037bd5085b0e9987d01ba37a6e0f
Irae Veras <irae.online@gmail.com>
2024-06-24 12:48:58 -0300

    refatoração na estrutura e styles

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
+import { RiErrorWarningFill } from "react-icons/ri";
 import { IoWarning } from "react-icons/io5";
 import "./ErrorPage.css";
 function ErrorPage() {
     return (
         <div className="page_error_404">
-            <IoWarning/>
-            <p>Error</p>
-            <h1>404</h1>            
+            <h1>404</h1>
+            <h3><RiErrorWarningFill/> <span>Ops! Página não encontrada!</span></h3>
+            <p>A página que você requisitou não foi encontrada.</p>
+            <Link to="/">Voltar</Link>
         </div>
     )
 }
