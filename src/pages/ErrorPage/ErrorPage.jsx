16c4c939e14e8b8b4a0c6cab63c22eb18ee707d1
Irae Veras <irae.online@gmail.com>
2024-06-23 23:56:33 -0300

    styles da page error 404

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,12 @@
+import { IoWarning } from "react-icons/io5";
+import "./ErrorPage.css";
 function ErrorPage() {
     return (
-        <>
-            <h1>Error 404</h1>
-        </>
+        <div className="page_error_404">
+            <IoWarning/>
+            <p>Error</p>
+            <h1>404</h1>            
+        </div>
     )
 }
 
