711ccbf4f7323ea54af4f4c0dd82154ee2edf9a7
Irae Veras <irae.online@gmail.com>
2024-06-25 00:19:23 -0300

    inserido graficos no dashboard

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Cardwidget from "../Cardwidget/Cardwidget";
 import Cardcount from "../Cardcount/Cardcount";
+import BarChart from "../Charts/ChartBar";
 import { BsCartDash } from "react-icons/bs";
 import { FaRegMoneyBillAlt } from "react-icons/fa";
 import { LuArrowDownFromLine } from "react-icons/lu";
@@ -9,64 +10,78 @@ import { FiUser } from "react-icons/fi";
 import { FiUserCheck } from "react-icons/fi";
 import { IoDocumentTextOutline } from "react-icons/io5";
 import { IoDocumentOutline } from "react-icons/io5";
-import "./Dashboard.css"
+import { PieChart } from "../Charts/ChartPie";
+import "./Dashboard.css";
+
+
+
 function Dashboard() {
     return (
-        <div className="dashboard row">
-            <Cardwidget 
-            icon={<BsCartDash/>} 
-            value={'307144'} 
-            title={"Total de vendas vencidas"} 
-            classNameValue={"dash_widget dash_1"} 
-            />
+        <div className="dashboard">        
+            <div className="card-widget-dash">
+                <Cardwidget 
+                icon={<BsCartDash/>} 
+                value={'307144'} 
+                title={"Total de vendas vencidas"} 
+                classNameValue={"dash_widget dash_1"} 
+                />
 
-            <Cardwidget 
-            icon={<FaRegMoneyBillAlt/>} 
-            value={Number('4385')} 
-            title={"Total de vendas vencidas"} 
-            classNameValue={"dash_widget dash_2"} />
+                <Cardwidget 
+                icon={<FaRegMoneyBillAlt/>} 
+                value={Number('4385')} 
+                title={"Total de vendas vencidas"} 
+                classNameValue={"dash_widget dash_2"} />
 
-            <Cardwidget 
-            icon={<LuArrowDownFromLine/>} 
-            value={Number('385656')} 
-            title={"Valor total da venda"} 
-            classNameValue={"dash_widget dash_3"} 
-            />
+                <Cardwidget 
+                icon={<LuArrowDownFromLine/>} 
+                value={Number('385656')} 
+                title={"Valor total da venda"} 
+                classNameValue={"dash_widget dash_3"} 
+                />
 
-            <Cardwidget 
-            icon={<LuArrowUpFromLine/>} 
-            value={'400000'} 
-            title={"Valor total da venda"} 
-            classNameValue={"dash_widget dash_4"} 
-            />
+                <Cardwidget 
+                icon={<LuArrowUpFromLine/>} 
+                value={'400000'} 
+                title={"Valor total da venda"} 
+                classNameValue={"dash_widget dash_4"} 
+                />
 
-            <Cardcount 
-            count={"100"} 
-            title={"Clientes"} 
-            icon={<FiUser/>} 
-            classNameValue={"dash_count dash_count_01"} 
-            />
+                <Cardcount 
+                count={"100"} 
+                title={"Clientes"} 
+                icon={<FiUser/>} 
+                classNameValue={"dash_count dash_count_01"} 
+                />
 
-            <Cardcount 
-            count={"100"} 
-            title={"Fornecedores"} 
-            icon={<FiUserCheck/>} 
-            classNameValue={"dash_count dash_count_02"} 
-            />
+                <Cardcount 
+                count={"100"} 
+                title={"Fornecedores"} 
+                icon={<FiUserCheck/>} 
+                classNameValue={"dash_count dash_count_02"} 
+                />
 
-            <Cardcount 
-            count={"100"} 
-            title={"Nota fiscal de compra"} 
-            icon={<IoDocumentTextOutline/>} 
-            classNameValue={"dash_count dash_count_03"} 
-            />
+                <Cardcount 
+                count={"100"} 
+                title={"Nota fiscal de compra"} 
+                icon={<IoDocumentTextOutline/>} 
+                classNameValue={"dash_count dash_count_03"} 
+                />
 
-            <Cardcount 
-            count={"105"} 
-            title={"Fatura de vendas"} 
-            icon={<IoDocumentOutline/>} 
-            classNameValue={"dash_count dash_count_04"} 
-            />
+                <Cardcount 
+                count={"105"} 
+                title={"Fatura de vendas"} 
+                icon={<IoDocumentOutline/>} 
+                classNameValue={"dash_count dash_count_04"} 
+                />
+            </div>
+            <div className="chart-dash">
+                <div className="chart-dash-bar">
+                    <BarChart />
+                </div>
+                <div className="chart-dash-bar">
+                <BarChart />
+                </div>
+            </div>
         </div>
     )
 }
