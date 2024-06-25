3057290d3a10bb050e74ddb90d70b5aef73a7ec0
Irae Veras <irae.online@gmail.com>
2024-06-25 00:14:25 -0300

    Criado graficos para dashboard

---
+++ src/Components/Charts/ChartBar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+function BarChart() {
+
+    const data = {
+        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        datasets: [
+            {
+                label: 'Total de vendas',
+                backgroundColor: 'rgba(75,192,192,0.2)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(75,192,192,0.4)',
+                hoverBorderColor: 'rgba(75,192,192,1)',
+                data: [65, 59, 80, 81, 56, 55, 40, 90, 115, 36, 59, 87],
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart',
+            },
+        },
+    };
+    
+    return (
+        <>
+            <Bar data={data} options={options} />;
+        </>
+    )
+}
+
+export default BarChart
