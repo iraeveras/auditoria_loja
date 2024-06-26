d139ee4fdad3f0889dbed4fbc478be6105aca5c5
Irae Veras <irae.online@gmail.com>
2024-06-25 23:16:20 -0300

    inserindo componente de grafico no dashboard

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import { Chart } from 'react-google-charts';
 import Cardwidget from "../Cardwidget/Cardwidget";
 import Cardcount from "../Cardcount/Cardcount";
-import BarChart from "../Charts/ChartBar";
 import { BsCartDash } from "react-icons/bs";
 import { FaRegMoneyBillAlt } from "react-icons/fa";
 import { LuArrowDownFromLine } from "react-icons/lu";
@@ -10,12 +10,20 @@ import { FiUser } from "react-icons/fi";
 import { FiUserCheck } from "react-icons/fi";
 import { IoDocumentTextOutline } from "react-icons/io5";
 import { IoDocumentOutline } from "react-icons/io5";
-import { PieChart } from "../Charts/ChartPie";
 import "./Dashboard.css";
 
 
 
 function Dashboard() {
+
+    const data = [
+        ["Element", "Density", { role: "style" }],
+        ["Copper", 8.94, "#b87333"], // RGB value
+        ["Silver", 10.49, "silver"], // English color name
+        ["Gold", 19.3, "gold"],
+        ["Platinum", 21.45, "color: #e5e4e2"], // CSS-style declaration
+    ];
+
     return (
         <div className="dashboard">        
             <div className="card-widget-dash">
@@ -76,11 +84,16 @@ function Dashboard() {
             </div>
             <div className="chart-dash">
                 <div className="chart-dash-bar">
-                    <BarChart />
+                    <Chart 
+                    chartType="ColumnChart" 
+                    width="100%" 
+                    height="400px" 
+                    data={data} 
+                    />
                 </div>
-                <div className="chart-dash-bar">
-                <BarChart />
-                </div>
+                {/* <div className="chart-dash-bar">
+                
+                </div> */}
             </div>
         </div>
     )
