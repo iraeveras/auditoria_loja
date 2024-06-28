a3aa7ef70970ca6c22a72376d10bf215812e1a8d
Irae Veras <irae.online@gmail.com>
2024-06-27 21:43:22 -0300

    criado arquivo para armazenar constantes

---
+++ src/constants/variables.js
@@ -0,0 +1,26 @@
+const listaCadastros = [
+    'CadEmpresa', 
+    'CadUser', 
+    'CadLoja', 
+    'CadAuditor', 
+    'CadCausa', 
+    'CadTipoDocReg', 
+    'CadCausaPerda', 
+    'CadTipoAvaliacaoOper', 
+    'CadQuestoes', 
+    'CadFrasesPadrao', 
+    'CadManutAuditoria', 
+    'CadVendasInformadas', 
+    'CadGrupos'
+]
+
+const listaRelatorios = [
+    'RelEmpresa', 
+    'RelUser', 
+    'RelLojas', 
+    'RelVendasInformadas', 
+    'RelGrupos', 
+    'RelCausaPerdas'
+]
+
+export {listaCadastros, listaRelatorios}
